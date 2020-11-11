641fa249e38949a51db420099c5b20968926316e
Ku Jin Shin <kujin.shin@gmail.com>
2020-11-10 20:54:37 -0500

    Rotate array - in-place solution

--- python/rotate_array.py
+++ python/rotate_array.py
@@ -25,3 +25,38 @@ class Solution:
             if new_i >= nums_length:
                 new_i = new_i - nums_length
             nums[new_i] = temp[i]
+
+
+from fractions import gcd
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        In-place Solution
+
+        Time complexity: O(n)
+        Space complexity: O(1)
+
+        - Here we shift by `k` amount until a cycle is completed
+          (ie. we return to the starting index)
+        - There are `gcd(len(nums), k)` cycles to go through
+        """
+        nums_length = len(nums)
+
+        # No need to rotate
+        if nums_length == 1:
+            return
+
+        # No need to rotate more than the length of an array
+        k =  k % nums_length
+
+        cycles = gcd(nums_length, k)
+
+        for starting_index in range(cycles):
+            index = starting_index
+            first_loop = True
+            temp = nums[index]
+            while index != starting_index or first_loop:
+                first_loop = False
+                next_index = (index + k) % nums_length
+                temp, nums[next_index] = nums[next_index], temp
+                index = next_index
